ccbdeadac843322a53e664c8d3f268955a628088
igorcdemenezes <igorcdemenezes@gmail.com>
2024-09-24 10:07:20 -0300

    Add project files.

---
+++ AT2dnweb/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AT2dnweb
+{
+    internal class Program
+    {
+        delegate void Deleg(string frase);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva a frase a ser convertida:");
+            string fraseDigitada = Console.ReadLine();
+
+            Deleg fraseMaiuscula = delegate (string frase)
+            {
+                Console.WriteLine("Conversão para maiúsculo: " + frase.ToUpper());
+            };
+
+            Deleg fraseMinuscula = delegate (string frase)
+            {
+                Console.WriteLine("Conversão para minúsculo: " + frase.ToLower());
+            };
+
+            fraseMaiuscula(fraseDigitada);
+            fraseMinuscula(fraseDigitada);
+        }
+    }
+}
